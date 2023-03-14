68541f862eac20d499d3ee41d6da78a3dc073867
Jack Jewell <jackjewell09@gmail.com>
2023-03-14 17:56:34 -0400

    [Bryce] Fix launching chrome

--- dashboard/rundashboard.bat
+++ dashboard/rundashboard.bat
@@ -1,5 +1,5 @@
 cd .\src
 start /min ..\bin\pynetworktables2js.exe --team 340 --port 8000
 
-start "" "%programfiles%\Google\Chrome\Application\chrome.exe" --app="http://localhost:8000" --window-position=1920,0 --kiosk --user-data-dir=%HOMEPATH%/dashboard0
-start "" "%programfiles%\Google\Chrome\Application\chrome.exe" --app="http://localhost:8000/joel.html" --window-position=-1920,0 --kiosk --user-data-dir=%HOMEPATH%/dashboard1
+start "" "%programfiles%\Google\Chrome\Application\chrome.exe" --app="http://localhost:8000" --window-position=1920,0 --kiosk --user-data-dir="%HOMEPATH%/dashboard0"
+start "" "%programfiles%\Google\Chrome\Application\chrome.exe" --app="http://localhost:8000/joel.html" --window-position=-1920,0 --kiosk --user-data-dir="%HOMEPATH%/dashboard1"
